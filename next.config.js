6de2638116fdf657f8a97d3b50dcf80bcd7adaa0
Kyle Cortez <32287606+kyle4346@users.noreply.github.com>
2023-04-28 17:09:25 -0500

    Changing typewriter Text animation

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
+  images: {
+    domains: ['th.bing.com','www.characterlives.org'] 
+  }
 }
 
 
-module.exports = nextConfig
+module.exports = nextConfig 
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,8 @@ import SplitType from "split-type";
 import { useEffect } from "react";
 import gsap from "gsap";
 import React from "react";
-// import ReactPlayer from "react-player/youtube";
 import dynamic from "next/dynamic";
-// import videoFilePath from "../../public/particle-5187.mp4"
+import Image from "next/image";
 
 const ReactPlayer = dynamic(() => import("react-player"), { ssr: false });
 
@@ -33,18 +32,33 @@ export default function Home() {
       repeat: 2,
       yoyo: true,
     });
+
+    const hiddenElements = document.querySelectorAll(".hid");
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+          entry.target.classList.add("show");
+        } else {
+          entry.target.classList.remove("show");
+        }
+      });
+    });
+
+    hiddenElements.forEach((el) => observer.observe(el));
   }, []);
-  <ReactPlayer
-    url="https://vimeo.com/460600541"
-    playing={true}
-    loop={true}
-    controls={false}
-    muted={true}
-    style={{
-      minWidth: "50%",
-      position: "absolute",
-    }}
-  />;
+
+  // <ReactPlayer
+  //   url="https://vimeo.com/460600541"
+  //   playing={true}
+  //   loop={true}
+  //   controls={false}
+  //   muted={true}
+  //   style={{
+  //     minWidth: "50%",
+  //     position: "absolute",
+  //   }}
+  // />;
   //https://www.youtube.com/watch?v=ysz5S6PUM-U
   return (
     <main className={orbitron.className}>
@@ -53,36 +67,59 @@ export default function Home() {
       </h1>
       {/* <div className="green"></div> */}
 
-      <section className="hidden">
-        <p>Software Engineer</p>
+      <section>
+        <div className="typewriter-wrapper">
+          <ul className="typewriter-list">
+            <li className="typewriter-text"><span>Software Engineer</span></li>
+            {/* <li className="typewriter-text"><span>Software Developer</span></li>
+            <li className="typewriter-text"><span>Circuit Designer</span></li>
+            <li className="typewriter-text"><span>Entrepreneur</span></li> */}
+          </ul>
+        </div>
       </section>
 
-      <section className="hidden">
-        <h3>Dedication</h3>
-        <p>
-          Learning multiple programming languages and designing complex circuits
-          throughout my career both in academia and personally is what inspires
-          me to keep moving forward, despite the hardships that may come
-        </p>
-        <h3>Ambitious</h3>
-        <p>
-          My passion to become a tech entrepreneur is a dream I&apos;ve had
-          since I was young. Turning my ideas into something that people can use
-          to help themselves is what I pride myself in doing no matter the
-          outcome. The COVID pandemic has truly tested my wits but I&apos;m glad
-          to still be working on myself after a while.
-        </p>
-        <h3>Goal-Oriented</h3>
-        <p>
-          I always keep myself focused at the task at hand and ensure that it is
-          done in a timely-manner. From developing web-apps to designing
-          circuits, I make sure that my tasks are done on or before it&apos;s
-          due. Everyone&apos;s had their ups and downs but if the going gets
-          tough, I work harder.
-        </p>
+      <section className="hid">
+        <div className="logos">
+          <div className="logo hid">
+            <h3>Commitment</h3>
+            <Image
+              src="https://www.characterlives.org/wp-content/uploads/2019/06/power-of-commitment.jpg"
+              alt="Work Hard, Play Hard"
+              width="100"
+              height="100"
+              className="image"
+            />
+            <p>
+              Learning multiple programming languages and designing complex
+              circuits throughout my career both in academia and personally is
+              what inspires me to keep moving forward, despite the hardships
+              that may come
+            </p>
+          </div>
+          <div className="logo hid">
+            <h3>Ambitious</h3>
+            <p>
+              My passion to become a tech entrepreneur is a dream I&apos;ve had
+              since I was young. Turning my ideas into something that people can
+              use to help themselves is what I pride myself in doing no matter
+              the outcome. The COVID pandemic has truly tested my wits but
+              I&apos;m glad to still be working on myself after a while.
+            </p>
+          </div>
+          <div className="logo hid">
+            <h3>Goal-Oriented</h3>
+            <p>
+              I always keep myself focused at the task at hand and ensure that
+              it is done in a timely-manner. From developing web-apps to
+              designing circuits, I make sure that my tasks are done on or
+              before it&apos;s due. Everyone&apos;s had their ups and downs but
+              if the going gets tough, I work harder.
+            </p>
+          </div>
+        </div>
       </section>
 
-      <section className="hidden">
+      <section className="hid">
         <h3>About Me</h3>
         <p>
           Hi, my name is Kyle Cortez and I&apos;m a recent tech grad currently
@@ -97,7 +134,7 @@ export default function Home() {
         </p>
       </section>
 
-      <section className="hidden">
+      <section className="hid">
         <h3>Resume</h3>
         <h4>Skills</h4>
         <p className="logo">
